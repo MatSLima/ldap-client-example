324aeca20e70b6226d27e41a04a13f0d7c50b1ed
Matheus <matsdelima@gmail.com>
2020-12-18 01:00:21 -0300

    add ldap-client

---
+++ LDAPClient/Client.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Novell.Directory.Ldap;
+using DirectoryEntry = System.DirectoryServices.DirectoryEntry;
+
+namespace LDAPClient2
+{
+    public class LDAP
+    {
+        public LdapConnection cn { get; set; }
+
+        public LDAP() => cn = new LdapConnection();
+        
+        public void CreateUser(string username, string password)
+        {
+            string domain = string.Format("uid={0},ou=users,dc=jogoglobal,dc=com", username);
+            
+            cn.Connect("localhost", 389);
+            cn.Bind("uid=admin,ou=system", "secret");
+
+            LdapAttributeSet attrs = new LdapAttributeSet();
+            attrs.Add(new LdapAttribute("uid", username));
+            attrs.Add(new LdapAttribute("userPassword", password));
+            attrs.Add(new LdapAttribute("ou", "users"));
+            attrs.Add(new LdapAttribute("objectClass", new string[] { "top", "account", "simpleSecurityObject" }));
+            
+            LdapEntry entry = new LdapEntry(domain, attrs);
+            cn.Add(entry);
+        }
+        
+        public bool Authenticate(string username, string password)
+        {
+            string domain = string.Format("uid={0},ou=users,dc=jogoglobal,dc=com", username);
+            LdapAttribute passwordAttr = new LdapAttribute("userPassword", password);
+            cn.Connect("localhost", 389);
+            cn.Bind("uid=admin,ou=system", "secret");
+            return cn.Compare(domain, passwordAttr);
+        }
+    }
+}
---
+++ LDAPClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LDAPClient2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LDAP ldap = new LDAP();
+            
+            ldap.CreateUser(username: "test", password: "123456");
+            bool authenticated = ldap.Authenticate(username: "test", password: "123456");
+            
+            Console.WriteLine(string.Format("Is authenticated: {0}", authenticated));
+        }
+    }
+}
